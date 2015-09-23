8962236b440f7be3cdc44f00874ef168cd5a3c44
Beraldo Leal <beraldo@ncc.unesp.br>
2015-09-23 08:58:28 -0300

    fixed frontend interface bug on right menu sidebar

--- webserver/web.py
+++ webserver/web.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.phtml", name = "Beraldo")
+    return render_template("index.html", name = "Beraldo")
 
 @app.route('/node/<node_id>')
 def node(node_id):
     return "Hello node %s" % node_id
 
 if __name__ == "__main__":
+
     app.run(host="0.0.0.0", debug=True)
